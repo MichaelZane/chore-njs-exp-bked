532bd9f749a3881c45a47d21253357d1a9e75aab
mzstevens69 <stevensmike36@yahoo.com>
2020-10-16 17:43:14 -0500

    changed 200 status on login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -68,7 +68,7 @@ router.post('/login', (req, res) => {
 
         res.status(200).json({
           message: `Welcome ${username} you are logged in !`,
-          token, user_id, username
+          token, user_id
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -93,7 +93,7 @@ router.post('/login/child', (req, res) => {
 
         res.status(200).json({
           message: `Hello, ${username} you have just logged in`,
-          token, parent_id, user_id, 
+          token, parent_id 
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
