18569e82189579d211f918fbdf7554b0eb1d27ad
mzstevens69 <stevensmike36@yahoo.com>
2020-10-18 21:46:34 -0500

    added vim support on heroku

--- parent/parent-router.js
+++ parent/parent-router.js
@@ -29,9 +29,9 @@ router.get('/:id', authenticate, (req, res) => {
       Parent.getChildById(id) // if child is found then get child by id
         .then(child => {
           let addChild = []
-          if(child.length) {
+          if(child.length) 
             addChild = child// if child exists add it to array
-          } else return "No children yet"
+
           res.status(200).json({ child: addChild }) // return array of parents children
         })
         .catch(err => {
