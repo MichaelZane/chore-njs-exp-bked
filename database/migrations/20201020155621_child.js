1ca0a34ec464b5fef07c62c1565eba373fee4471
mzstevens69 <stevensmike36@yahoo.com>
2020-10-20 16:24:27 -0500

    database seeds

--- database/migrations/20201020155621_child.js
+++ database/migrations/20201020155621_child.js
@@ -3,29 +3,29 @@ exports.up = function(knex) {
     return (
     	knex.schema
     //child table
-				.createTable("child", tbl => {
-						tbl.increments();
+			.createTable("child", tbl => {
+					tbl.increments();
 
-						tbl.string("fstname").notNullable();
+					tbl.string("fstname").notNullable();
 
-						tbl.string("lstname").notNullable();
+					tbl.string("lstname").notNullable();
 
-						tbl
-						.string("username", 128)
-						.notNullable()
-						.unique();
+					tbl
+					.string("username", 128)
+					.notNullable()
+					.unique();
 
-						tbl.string("password", 128).notNullable()
+					tbl.string("password", 128).notNullable()
 
-						tbl
-						.integer("parent_id")
-						.unsigned()
-						.references("id")
-						.inTable("parent")
-						.onDelete("RESTRICT")
-						.onUpdate("CASCADE");
+					tbl
+					.integer("parent_id")
+					.unsigned()
+					.references("id")
+					.inTable("parent")
+					.onDelete("RESTRICT")
+					.onUpdate("CASCADE");
 
-				})
+			})
     )
 };
 
