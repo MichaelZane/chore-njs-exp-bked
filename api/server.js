274793d795ec49443a7277811d95787d2a6278c9
mzstevens69 <stevensmike36@yahoo.com>
2021-01-10 11:10:04 -0600

    image upload

--- api/server.js
+++ api/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+require('dotenv').config()
+
+const cloudinary = require('cloudinary')
+const formData = require('express-form-data')
 
 
 const childRouter = require('../child/child-router');
@@ -12,9 +16,27 @@ const choresRouter = require('../chores/chores-router');
 
 const server = express();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+})
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use(formData.parse())
+
+server.use('/image-upload', (req, res) => {
+  const values = Object.values(req.files)
+  const promises = values.map(image => cloudinary.uploader.upload(image.path))
+
+  Promise
+    .all(promises)
+    .then(results => res.json(results))
+    .catch((err) => res.status(400).json(err))
+})
 
 // routes
 
@@ -26,7 +48,7 @@ server.use('/api/chore', choresRouter);
 // server check
 
 server.get('/', (req, res) => {
-  res.status(200).json({ api: "If you see me, I am here..."});
+  res.status(200).json({  api: "If you see me, I am here..."});
 });
 
 module.exports = server;
--- chores/chores-router.js
+++ chores/chores-router.js
@@ -4,64 +4,6 @@ const Chores = require('./chores-model');
 
 const authenticate = require('../auth/authenticateMW');
 
-const AWS = require('aws-sdk');
-
-const fs = require('fs');
-
-require('dotenv').config()
-
-const express = require('express');
-
-//const fileType = require('file-type');
-
-const multiparty = require('multiparty');
-
-const server = express();
-
-// configure the keys for accessing AWS
-
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
-
-// create S3 instance
-
-const s3 = new AWS.S3();
-
-// abstracts function to upload a file returning a promise
-// NOTE: if you are using TypeScript the typed function signature will be
-// const uploadFile = (buffer: S3.Body, name: string, type: { ext: string; mime: string })
-const uploadFile = (buffer, name, type) => {
-  const params = {
-    ACL: 'public-read',
-    Body: buffer,
-    Bucket: process.env.S3_BUCKET,
-    ContentType: type.mime,
-    Key: `${name}.${type.ext}`,
-  };
-  return s3.upload(params).promise();
-};
-
-// Define POST route
-server.post('/upload', (req, res) => {
-  const form = new multiparty.Form();
-  form.parse(req, async (error, fields, files) => {
-    if (error) {
-      return res.status(500).send(error);
-    };
-    try {
-      const path = files.file[0].path;
-      const buffer = fs.readFileSync(path);
-      const type = await FileType.fromBuffer(buffer);
-      const fileName = `bucketFolder/${Date.now().toString()}`;
-      const data = await uploadFile(buffer, fileName, type);
-      return response.status(200).send(data);
-    } catch (err) {
-      return response.status(500).send(err);
-    }
-  });
-});
 
 // get a chore by id in database
 
