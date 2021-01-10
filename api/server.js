55fb6cec42ccf01502aa20116b95414ebd83f0d0
mzstevens69 <stevensmike36@yahoo.com>
2021-01-10 15:48:50 -0600

    fixed server.js error

--- api/server.js
+++ api/server.js
@@ -27,14 +27,17 @@ server.use('/api/chore', choresRouter);
 
 // server check
 
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"),
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     )
+//   next()
+// })
+
 server.get('/', (req, res) => {
   res.status(200).json({  api: "If you see me, I am here..."});
 });
 
-module.exports = server => {
-  server.use(
-    fileupload({
-      useTempFiles: true
-    })
-  )
-};
+module.exports = server 
