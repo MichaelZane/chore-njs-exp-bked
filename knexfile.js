eebbf3ca61ae32dcb68f1e0404b94140ae6dd6a7
Mike Stevens <angelhomeinspections@gmail.com>
2021-04-11 09:56:08 -0500

    *

--- knexfile.js
+++ knexfile.js
@@ -31,7 +31,9 @@ module.exports = {
     client: "pg",
     connection: {
       connectionString: process.env.DATABASE_URL, 
-     
+      ssl: {
+        rejectUnauthorized: false
+      }
     },
     migrations: {
       directory: "./database/migrations"
