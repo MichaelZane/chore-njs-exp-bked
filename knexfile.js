7cf4d0baba77d7e5f31820c2c206868529ae8722
MichaelZane <stevensmike36@yahoo.com>
2021-04-27 22:15:58 -0500

    changed acct

--- knexfile.js
+++ knexfile.js
@@ -21,11 +21,6 @@ module.exports = {
     seeds: {
       directory: "./database/seeds"
     },
-    // pool: {
-    //   afterCreate: (conn, done) => {
-    //     conn.run("PRAGMA foreign_keys = ON", done)
-    //   }
-    // }
   },
   production: {
     client: "pg",
