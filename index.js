3d1b3f8141996a58b8ff2a1f7398befd0c799d19
MichaelZane <stevensmike36@yahoo.com>
2021-06-26 00:44:08 -0500

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 const server = require('./api/server.js');
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+server.listen(PORT, (err) => {
+  if(err) return console.error(err)
   console.log(`\n<=*=> Rocking it on port ${PORT} <=*=>\n`);
 });
 
--- knexfile.js
+++ knexfile.js
@@ -19,10 +19,10 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: process.env.HOST,
-      database: process.env.DATABASE,
-      user: process.env.USER,
-      password: process.env.PASSWORD
+      // host: process.env.HOST,
+      // database: process.env.DATABASE,
+      // user: process.env.USER,
+      // password: process.env.PASSWORD
     },
 //    connection: 'postgres://localhost/postgres',
     pool: {
